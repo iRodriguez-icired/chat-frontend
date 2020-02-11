d3df344e583ad8896f70533679acc45e75ba8d04
Antonio <tenlla2@gmail.com>
2020-02-11 01:01:59 +0100

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import router from '../router';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    url: 'http://localhost:3000',
+    name: '',
+    rooms: [],
+    messages: [],
+    state: false,
   },
   mutations: {
+    setNombre: (state, name) => {
+      state.name = name;
+      router.push('/rooms', () => { });
+    },
+
+    setRooms: (state, setRooms) => {
+      state.rooms = setRooms;
+    },
+    setMsg: (state, msg) => {
+      state.messages = msg;
+    },
+    setState: (state, bool) => {
+      state.state = bool;
+    },
+
   },
   actions: {
   },
   modules: {
+  },
+
+  getters: {
+    // eslint-disable-next-line no-underscore-dangle
+    getRoomName: ((state) => (id) => state.rooms.find((room) => room._id.$oid === id)),
   },
 });
