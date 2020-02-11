81fc168e60fcc3172d9068fb2df3229119223ca1
Antonio <tenlla2@gmail.com>
2020-02-11 01:53:16 +0100

    actioncable

--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Toasted from 'vue-toasted';
+import ActionCableVue from 'actioncable-vue';
+import { BootstrapVue } from 'bootstrap-vue';
+import VueResource from 'vue-resource';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import store from './store';
+import router from './router';
+import App from './App.vue';
 import i18n from './locales';
 
 Vue.config.productionTip = false;
 
+Vue.use(Toasted);
+Vue.use(ActionCableVue, {
+  debug: true,
+  debugLevel: 'error',
+  connectionUrl: 'ws://localhost:3000/chat',
+  connectImmediately: true,
+});
+Vue.use(BootstrapVue);
+Vue.use(VueResource);
+
 new Vue({
+  i18n,
   router,
   store,
-  i18n,
   render: (h) => h(App),
 }).$mount('#app');
