e4f1ce3038f7f0ac012928ea319b087c19c14516
IciredAtenllado <atenllado@iciredimpagados.com>
2020-03-04 10:13:19 +0100

    action cable data formated

--- src/js/services/action-cable.js
+++ src/js/services/action-cable.js
@@ -12,16 +12,17 @@ export default {
     if (msg !== '') {
       Vue.prototype.$cable.perform({
         channel: 'RoomChannel',
-        data: {
-          content: {
-            author: name,
-            text: msg,
-            room_id: param
-          }
-        }
+        data: this.parseData(msg, name, param)
       });
     } else {
       Vue.toasted.error(router.app.$t('err2'));
     }
+  },
+  parseData(msg, name, param) {
+    return JSON.stringify({
+      author: name,
+      text: msg,
+      room_id: param
+    });
   }
 };
