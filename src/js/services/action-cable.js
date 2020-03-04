d31d38607e4d4c92a7b3acfabe39dd4484c2c7aa
IciredAtenllado <atenllado@iciredimpagados.com>
2020-03-04 11:56:00 +0100

    changes reverted action cable

--- src/js/services/action-cable.js
+++ src/js/services/action-cable.js
@@ -12,17 +12,16 @@ export default {
     if (msg !== '') {
       Vue.prototype.$cable.perform({
         channel: 'RoomChannel',
-        data: this.parseData(msg, name, param)
+        data: {
+          content: {
+            author: name,
+            text: msg,
+            room_id: param
+          }
+        }
       });
     } else {
       Vue.toasted.error(router.app.$t('err2'));
     }
-  },
-  parseData(msg, name, param) {
-    return JSON.stringify({
-      author: name,
-      text: msg,
-      room_id: param
-    });
   }
 };
